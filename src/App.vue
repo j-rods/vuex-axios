9370cb36fc36b5f34c444c6b5af3efc1856d9add
j-rods <jrrodriguez84@gmail.com>
2019-07-22 02:51:51 +0100

    set up basic test in app vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ msg }}
   </div>
 </template>
 
@@ -10,8 +9,10 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      msg: "hello"
+    }
   }
 }
 </script>
