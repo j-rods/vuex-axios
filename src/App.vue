738a60ea2037ab43bd04c34e73b501e116a34879
j-rods <jrrodriguez84@gmail.com>
2019-07-22 04:42:49 +0100

    import mapState, render posts title

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
-    
+    <h5 v-for="post in posts" v-bind:key="post.id">{{post.title}}</h5>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 
 export default {
+
   name: 'app',
-    mounted() {
-      this.$store.dispatch('loadPosts')
+    computed: {
+      ...mapState([
+          'posts'
+      ])
     }
 }
 </script>
