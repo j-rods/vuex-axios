bd22eb35555423382bf5f14d21ab1b2489edfd9d
j-rods <jrrodriguez84@gmail.com>
2019-07-23 02:41:07 +0100

    add ui template

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,33 @@
 <template>
   <div id="app">
-    <h5 v-for="post in posts" v-bind:key="post.id">{{post.title}}</h5>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-toolbar color="pink" dark>
+            <v-toolbar-side-icon></v-toolbar-side-icon>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+              <v-icon>search</v-icon>
+            </v-btn>
+            <v-btn icon>
+              <v-icon>check_circle</v-icon>
+            </v-btn>
+          </v-toolbar>
+
+          <v-list two-line>
+            <template v-for="(post, index) in posts">
+              <v-list-tile :key="post.title" avatar ripple @click="toggle(index)">
+                <v-list-tile-content>
+                  <v-list-tile-title>{{ post.title }}</v-list-tile-title>
+                  <v-list-tile-sub-title>{{ post.body }}</v-list-tile-sub-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <v-divider v-if="index + 1 < posts.length" :key="index"></v-divider>
+            </template>
+          </v-list>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
@@ -9,13 +36,21 @@ import { mapState } from 'vuex'
 
 export default {
   name: 'app',
-  mounted () {
+  mounted() {
     this.$store.dispatch('loadPosts')
   },
   computed: {
-    ...mapState([
-        'posts'
-    ])
+    ...mapState(['posts'])
+  },
+  methods: {
+    toggle(index) {
+      const i = this.selected.indexOf(index)
+      if (i > -1) {
+        this.selected.splice(i, 1)
+      } else {
+        this.selected.push(index);
+      }
+    }
   }
 }
 </script>
