7b3312cc3129117da6db020c711e93f04d02ae8f
j-rods <jrrodriguez84@gmail.com>
2019-07-23 06:46:22 +0100

    install material icons

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import App from './App.vue'
 import store from './store'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {iconfont: 'md'})
 
 Vue.config.productionTip = false
 
