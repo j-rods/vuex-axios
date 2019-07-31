c4c01bd57db2ff86356eb1674313dd90cb5c5bc5
j-rods <jrrodriguez84@gmail.com>
2019-07-31 16:03:39 +0100

    import vuex in main js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
 import 'es6-promise/auto';
 import Vue from 'vue'
-import App from './components/App.vue'
-import store from './store'
+import Vuex from 'vuex'
+import App from '@/components/App.vue'
+import store from '@/store'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.use(Vuetify, {iconfont: 'md'})
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
 new Vue({
-  el: '#app',
   store,
   render: h => h(App)
-})
+}).$mount('#app')
