fbb0acf3284b4fcf29d5f3ce935e0ae12ce139e6
j-rods <jrrodriguez84@gmail.com>
2019-08-02 12:40:30 +0100

    fix formatting issues in mainjs

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 import 'es6-promise/auto';
-import Vue from 'vue'
-import Vuex from 'vuex'
-import App from './App.vue'
-import store from '@/store/index.js'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import App from './App.vue';
+import store from './store/index';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
-Vue.use(Vuetify, {iconfont: 'md'})
-Vue.use(Vuex)
+Vue.use(Vuetify, { iconfont: 'md' });
+Vue.use(Vuex);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
